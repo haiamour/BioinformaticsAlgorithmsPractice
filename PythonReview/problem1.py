84bff0b67a59be0c888365c51832e911c18c9f44
heathyates <hlyates@ksu.edu>
2015-01-17 22:53:26 -0600

    Added python review folder and completed two exercise. Very basic stuff

---
+++ PythonReview/problem1.py
@@ -0,0 +1,9 @@
+#Author: Heath Yates 
+#Description: Take two integers a and b, less than 1000 and return square of hypotenuse of the right traingle 
+#whose legs have lengths a and b 
+
+a = 945 
+b = 919 
+c = a*a + b*b
+
+print "The square of hypotenuse is ", c
---
+++ PythonReview/problem2.py
@@ -0,0 +1,10 @@
+#Author: Heath Yates 
+#Description: Given a string of length 200 letters, slice string from place a to place b and c through d 
+
+sentence = ` 
+a = 
+b = 
+c = 
+d = 
+slice = sentence[a:b] + sentence[c:d]
+print slice 
